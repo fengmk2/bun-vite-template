40c27cc25425fd651f00c047ff6b58e811ecc263
Lewis_Damy <ldamy16@gmail.com>
2026-01-31 01:23:52 +0000

    no change

--- eslint.config.js
+++ eslint.config.js
@@ -1,22 +1,23 @@
-import mantine from 'eslint-config-mantine';
-import { defineConfig } from 'eslint/config';
-import tseslint from 'typescript-eslint';
+import js from '@eslint/js'
+import globals from 'globals'
+import reactHooks from 'eslint-plugin-react-hooks'
+import reactRefresh from 'eslint-plugin-react-refresh'
+import tseslint from 'typescript-eslint'
+import { defineConfig, globalIgnores } from 'eslint/config'
 
-// @ts-check
-export default defineConfig(
-  tseslint.configs.recommended,
-  ...mantine,
-  { ignores: ['**/*.{mjs,cjs,js,d.ts,d.mts}'] },
+export default defineConfig([
+  globalIgnores(['dist']),
   {
-    files: ['**/*.story.tsx'],
-    rules: { 'no-console': 'off' },
-  },
-  {
+    files: ['**/*.{ts,tsx}'],
+    extends: [
+      js.configs.recommended,
+      tseslint.configs.recommended,
+      reactHooks.configs.flat.recommended,
+      reactRefresh.configs.vite,
+    ],
     languageOptions: {
-      parserOptions: {
-        tsconfigRootDir: process.cwd(),
-        project: ['./tsconfig.json'],
-      },
+      ecmaVersion: 2020,
+      globals: globals.browser,
     },
-  }
-);
+  },
+])
