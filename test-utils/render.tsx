e94881b2b7b068b3a67b1b2a6bbf9d91d08f3016
Lewis_Damy <ldamy16@gmail.com>
2026-01-31 01:35:13 +0000

    fix: update test render utility types

--- test-utils/render.tsx
+++ test-utils/render.tsx
@@ -1,13 +1,12 @@
+import type { ReactNode } from 'react';
 import { render as testingLibraryRender } from '@testing-library/react';
 import { MantineProvider } from '@mantine/core';
-import { theme } from '../src/theme';
+import { theme } from '@/theme.ts';
 
-export function render(ui: React.ReactNode) {
+export function render(ui: ReactNode) {
   return testingLibraryRender(ui, {
-    wrapper: ({ children }: { children: React.ReactNode }) => (
-      <MantineProvider theme={theme} env="test">
-        {children}
-      </MantineProvider>
+    wrapper: ({ children }: { children: ReactNode }) => (
+      <MantineProvider theme={theme}>{children}</MantineProvider>
     ),
   });
 }
