2accb4803d5eae4bd16cb27f755462a61aa0494c
Lewis_Damy <ldamy16@gmail.com>
2026-01-31 01:34:56 +0000

    chore: migrate to mantine eslint config

--- eslint.config.js
+++ eslint.config.js
@@ -1,23 +1,20 @@
-import js from '@eslint/js'
-import globals from 'globals'
-import reactHooks from 'eslint-plugin-react-hooks'
-import reactRefresh from 'eslint-plugin-react-refresh'
-import tseslint from 'typescript-eslint'
-import { defineConfig, globalIgnores } from 'eslint/config'
+import mantine from 'eslint-config-mantine';
 
-export default defineConfig([
-  globalIgnores(['dist']),
+export default [
+  ...mantine,
   {
-    files: ['**/*.{ts,tsx}'],
-    extends: [
-      js.configs.recommended,
-      tseslint.configs.recommended,
-      reactHooks.configs.flat.recommended,
-      reactRefresh.configs.vite,
+    ignores: [
+      'dist',
+      'storybook-static',
+      'vite.config.ts',
+      'vitest.setup.mjs',
+      '**/*.{mjs,cjs,js,d.ts,d.mts}',
     ],
-    languageOptions: {
-      ecmaVersion: 2020,
-      globals: globals.browser,
+  },
+  {
+    files: ['**/*.story.tsx'],
+    rules: {
+      'no-console': 'off',
     },
   },
-])
+];
